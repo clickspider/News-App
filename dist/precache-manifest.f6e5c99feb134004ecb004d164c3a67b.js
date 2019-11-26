420903a3debd3d349f57f66d81e5ba5a83dfafec
clickspider <danielfrey101@gmail.com>
2019-11-26 21:04:47 +0200

    Updated dist V1

---
+++ dist/precache-manifest.f6e5c99feb134004ecb004d164c3a67b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64ef9f0b0034b38412e",
+    "url": "/css/about.b9eb775b.css"
+  },
+  {
+    "revision": "74ea441a0780c62e5fca",
+    "url": "/css/app.7313742b.css"
+  },
+  {
+    "revision": "5d6a8d431ff577b8343a",
+    "url": "/css/chunk-vendors.f2399d54.css"
+  },
+  {
+    "revision": "cb294f8f46b79f589a6680ceaa4f03f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e64ef9f0b0034b38412e",
+    "url": "/js/about.791ab968.js"
+  },
+  {
+    "revision": "74ea441a0780c62e5fca",
+    "url": "/js/app.22548f8c.js"
+  },
+  {
+    "revision": "5d6a8d431ff577b8343a",
+    "url": "/js/chunk-vendors.55e23a26.js"
+  },
+  {
+    "revision": "1997ef556dd5d2f6907b28437151f6f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
