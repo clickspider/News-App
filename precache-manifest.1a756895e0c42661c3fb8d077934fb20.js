bf7c56a9e0a963e5e87a59ca62c2dac8c5aaf6bb
clickspider <danielfrey101@gmail.com>
2019-11-27 17:31:22 +0200

    Updated dist V1.3 - offline mode ON

---
+++ precache-manifest.1a756895e0c42661c3fb8d077934fb20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9426eedd72d15efc2434",
+    "url": "/css/about.17896133.css"
+  },
+  {
+    "revision": "86729509c8878c8a6e5d",
+    "url": "/css/app.0a3a05c4.css"
+  },
+  {
+    "revision": "3a64d63471a3f447c58c",
+    "url": "/css/chunk-vendors.f2399d54.css"
+  },
+  {
+    "revision": "76fac06f3514ccaee549b28b424aee0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9426eedd72d15efc2434",
+    "url": "/js/about.2a2088bd.js"
+  },
+  {
+    "revision": "86729509c8878c8a6e5d",
+    "url": "/js/app.754c774b.js"
+  },
+  {
+    "revision": "3a64d63471a3f447c58c",
+    "url": "/js/chunk-vendors.f0201584.js"
+  },
+  {
+    "revision": "1997ef556dd5d2f6907b28437151f6f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
